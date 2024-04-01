cfc44bcc13d34347fe3101f05c9c91aa958bfab6
Kiragu Maina <jumaliko711@gmail.com>
2024-04-01 16:07:50 +0300

    medicationtop

--- apis/models.py
+++ apis/models.py
@@ -7,6 +7,18 @@ from django.core.files import File
 from django.contrib.auth.models import User  # Import the User model
 
 
+
+class Medication(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    dosage = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Order(models.Model):
     STATUS_CHOICES = (
         ('pending', 'Pending'),
@@ -30,17 +42,6 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.medication.name}"
-
-        
-class Medication(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    dosage = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock_quantity = models.IntegerField()
-
-    def __str__(self):
-        return self.name
 
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
