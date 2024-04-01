6f11fb7487a41c882a863b6cde8490004a64b7af
Kiragu Maina <jumaliko711@gmail.com>
2024-04-01 11:15:50 +0300

    Update models.py

--- apis/models.py
+++ apis/models.py
@@ -6,7 +6,15 @@ from io import BytesIO
 from django.core.files import File
 
 
+class Medication(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    dosage = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
 
+    def __str__(self):
+        return self.name
 
 class Products(models.Model):
     itemName = models.CharField(max_length=255)
