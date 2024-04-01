9aeca4780cd55d39f6dcebeb77e70ea196bcaf04
Kiragu Maina <jumaliko711@gmail.com>
2024-04-01 11:16:55 +0300

    Update serializers.py

--- apis/serializers.py
+++ apis/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Products, Product, Shop
 
+from .models import Medication
+
+class MedicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Medication
+        fields = '__all__' 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
