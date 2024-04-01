885cc09ba29ca21f029ea0a49c094e5eaf923342
Kiragu Maina <jumaliko711@gmail.com>
2024-04-01 14:59:55 +0300

    quantity

--- apis/admin.py
+++ apis/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Product, Shop, Medication
+from .models import Product, Shop, Medication, Cart, CartItem
 
 admin.site.register(Product)
 admin.site.register(Shop)
 admin.site.register(Medication)
+admin.site.register(Cart)
+admin.site.register(CartItem)
 
 # Register your models here.
