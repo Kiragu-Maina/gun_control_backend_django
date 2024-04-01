7686fba25f6fbcd043894cbf85e8d7965b24ea85
Kiragu Maina <jumaliko711@gmail.com>
2024-04-01 20:19:19 +0300

    profile

--- apis/signals.py
+++ apis/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from .models import Profile
 # If you're using a custom user model, replace the above import with:
 # from django.conf import settings
 # User = settings.AUTH
