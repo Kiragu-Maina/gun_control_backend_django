7d4a03a97faa214edcd15eaeb8a89f3820b29bfe
Kiragu Maina <jumaliko711@gmail.com>
2024-04-01 19:14:24 +0300

    orders

--- apis/admin.py
+++ apis/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Product, Shop, Medication, Cart, CartItem
+from .models import Product, Shop, Medication, Cart, CartItem, Order
 
 admin.site.register(Product)
 admin.site.register(Shop)
 admin.site.register(Medication)
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Order)
 
 # Register your models here.
